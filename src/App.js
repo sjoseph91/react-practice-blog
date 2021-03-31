eefec43ad8d43fc414bdb9a3236262464fd7916e
Sean Joseph <sean_joseph@protonmail.com>
2021-03-30 21:27:57 -0700

    changed Readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//server npx json-server --watch data/db.json --port 8000
+//Finished # 24
+import React from 'react';
 
+import Navbar from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 function App() {
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+/* 
+Lesson 24 use Cleanup. An erro occurred when I quickly switched from my home component to my New Blog component.
+  This was because the fetch did not complete, and it thrown an error. To stop this from happening we need to associate 
+  that fetch with a new Abort Controller const abortCont = new AbortController();
+  fetch(url, { signal: abortCont.signal})
+  , and when cleaning up the use effect hook, we need to return return () => abortCont.abort();
+
+*/
